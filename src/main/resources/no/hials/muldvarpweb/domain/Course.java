bbaff6b8d5a4914cbc50ae7cabf7673420d24657
Kristoffer Strøm Bergset <kristoffer@bergset.no>
2012-02-07 13:43:00 +0100

    Mockup stuff

---
+++ src/main/resources/no/hials/muldvarpweb/domain/Course.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package no.hials.muldvarpweb.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author kristoffer
+ */
+@Entity
+@Table(name = "course")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Course.findAll", query = "SELECT c FROM Course c")})
+public class Course implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id")
+    private Short id;
+    String name;
+    String detail;
+    String imageurl;
+    Integer revision;
+    ArrayList<Theme> themes;
+}
---
+++ src/main/resources/no/hials/muldvarpweb/web/CourseController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package no.hials.muldvarpweb.web;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import no.hials.muldvarpweb.domain.Course;
+import no.hials.muldvarpweb.service.CourseService;
+
+/**
+ *
+ * @author kristoffer
+ */
+@Named
+@SessionScoped
+public class CourseController implements Serializable {
+    @Inject CourseService service;
+    Course newCourse;
+    List<Course> courses;
+    Course selected;
+    Course filter;
+}
