2b8d1bcf77764a20eb29442c93a2896a24f94675
Terje Wallem <nospherus01@gmail.com>
2012-03-20 02:07:09 +0100

    Put together a html site to add a document  to the server. No functionality is added yet.

--- src/main/java/no/hials/muldvarpweb/service/LibraryService.java
+++ src/main/java/no/hials/muldvarpweb/service/LibraryService.java
@@ -13,7 +13,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import no.hials.muldvarpweb.domain.Course;
 import no.hials.muldvarpweb.domain.LibraryItem;
 
 /**
@@ -82,9 +81,9 @@ public class LibraryService {
         }
         
         public void makeTestData(){
-            LibraryItem lI = new LibraryItem("A tale of two men", "How to make money", "Carl Brooke", "Austin Maxwell", "15.03.1998", "29.02.2012", "239", "This is a book explaining how a pair of people can make money", "http://unrestrictedstock.com/wp-content/uploads/office-icons-book-free-stock-vector.jpg", "http://whhs.cps-k12.org/library/images/icon_book.gif", "http://www.hials.no/nor/content/download/49530/977307/file/PB01%20ETABLERING%20AV%20NYE%20STUDIETILBUD%20TIL%20OG%20MED%2030%20STUDIEPOENG.pdf");
-            LibraryItem lI2 = new LibraryItem("Monster manual", "Defence against the black arts", "Severus Snape", "Harry Potter", "5.08.2008", "20.05.2012", "100", "This book explains how you can kick ass", "http://unrestrictedstock.com/wp-content/uploads/office-icons-book-free-stock-vector.jpg", "http://whhs.cps-k12.org/library/images/icon_book.gif", "http://www.hials.no/nor/content/download/49530/977307/file/PB01%20ETABLERING%20AV%20NYE%20STUDIETILBUD%20TIL%20OG%20MED%2030%20STUDIEPOENG.pdf");
-            LibraryItem lI3 = new LibraryItem("Master & Commander", "A wet movie", "Master B", "Commander D", "09.01.1460", "30.12.2011", "1902", "This book is a book about sailboats and domination", "http://unrestrictedstock.com/wp-content/uploads/office-icons-book-free-stock-vector.jpg", "http://whhs.cps-k12.org/library/images/icon_book.gif", "http://www.hials.no/nor/content/download/49530/977307/file/PB01%20ETABLERING%20AV%20NYE%20STUDIETILBUD%20TIL%20OG%20MED%2030%20STUDIEPOENG.pdf");
+            LibraryItem lI = new LibraryItem("A tale of two men", "How to make money", "Carl Brooke", "Austin Maxwell", "15.03.1998", "29.02.2012", "239", "This is a book explaining how a pair of people can make money", "http://unrestrictedstock.com/wp-content/uploads/office-icons-book-free-stock-vector.jpg", "http://whhs.cps-k12.org/library/images/icon_book.gif", "http://cran.r-project.org/doc/manuals/R-intro.pdf");
+            LibraryItem lI2 = new LibraryItem("Monster manual", "Defence against the black arts", "Severus Snape", "Harry Potter", "5.08.2008", "20.05.2012", "100", "This book explains how you can kick ass", "http://unrestrictedstock.com/wp-content/uploads/office-icons-book-free-stock-vector.jpg", "http://whhs.cps-k12.org/library/images/icon_book.gif", "http://cran.r-project.org/doc/manuals/R-intro.pdf");
+            LibraryItem lI3 = new LibraryItem("Master & Commander", "A wet movie", "Master B", "Commander D", "09.01.1460", "30.12.2011", "1902", "This book is a book about sailboats and domination", "http://unrestrictedstock.com/wp-content/uploads/office-icons-book-free-stock-vector.jpg", "http://whhs.cps-k12.org/library/images/icon_book.gif", "http://cran.r-project.org/doc/manuals/R-intro.pdf");
             addLibraryItem(lI);
             addLibraryItem(lI2);
             addLibraryItem(lI3);
