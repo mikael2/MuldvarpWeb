80a41fc94153c693761291303229220007e6e778
johan <johan@localhost>
2012-09-26 11:10:01 +0200

    New!

---
+++ src/main/java/no/hials/muldvarpweb/v2/NEW.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package no.hials.muldvarpweb.v2;
+
+/**
+ *
+ * @author johan
+ */
+public class NEW {
+    
+}
