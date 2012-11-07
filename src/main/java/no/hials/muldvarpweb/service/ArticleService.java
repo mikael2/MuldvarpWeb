5ca414764637602f026a6ff39675a0229dc969b1
Kristoffer Strøm Bergset <kristoffer@bergset.no>
2012-11-07 10:53:37 +0100

    updated for new server

--- src/main/java/no/hials/muldvarpweb/service/ArticleService.java
+++ src/main/java/no/hials/muldvarpweb/service/ArticleService.java
@@ -8,6 +8,8 @@ import javax.persistence.TypedQuery;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import no.hials.muldvarpweb.domain.Article;
 
 /**
@@ -17,36 +19,46 @@ import no.hials.muldvarpweb.domain.Article;
 @Stateless
 @Path("article")
 public class ArticleService {
-    
+
     @PersistenceContext
     EntityManager em;
-    
+
     @GET
     @Path("{id}")
+    @Produces({MediaType.APPLICATION_JSON})
     public Article getArticle (@PathParam("id") Long id) {
         TypedQuery<Article> q = em.createQuery("Select a from Article a where a.id = :id", Article.class);
         q.setParameter("id", id);
         return q.getSingleResult();
     }
-    
+
+    @GET
+    @Path("/news/")
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Article> findNews() {
+        TypedQuery<Article> q = em.createQuery("Select a from Article a where a.category LIKE :category", Article.class);
+        q.setParameter("category", "news");
+        return q.getResultList();
+    }
+
     public Article findArticle (String title) {
         TypedQuery<Article> q = em.createQuery("Select a from Article a where a.id = :id", Article.class);
         q.setParameter("title", title);
         return q.getSingleResult();
     }
-    
-    public List<Article> findArticles (String title) {       
+
+    public List<Article> findArticles (String title) {
         TypedQuery<Article> q = em.createQuery("Select a from Article a where a.title LIKE :title", Article.class);
         q.setParameter("name", "%" + title + "%");
         return q.getResultList();
     }
-    
-    public List<Article> findMostRecentArticles () {       
+
+    public List<Article> findMostRecentArticles () {
         TypedQuery<Article> q = em.createQuery("Select a from Article a", Article.class);
         return q.getResultList();
     }
-    
-    
+
+
     public Article addArticle(Article newArticle) {
         newArticle= em.merge(newArticle);
         em.persist(newArticle);
@@ -57,6 +69,6 @@ public class ArticleService {
         article = em.merge(article);
         em.remove(article);
     }
-    
-    
+
+
 }
