336e0ab7e734d7eb56c33444a0df860e486f6a75
Mikael Tollefsen <mikael@tollefsen.biz>
2012-12-03 16:11:33 +0100

    Moved templates to templates folder

---
+++ src/main/java/no/hials/muldvarpweb/web/ViewRestController.java
@@ -0,0 +1,30 @@
+package no.hials.muldvarpweb.web;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedProperty;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Mikael
+ */
+@Named
+@RequestScoped
+public class ViewRestController {
+    @ManagedProperty(value="#{param.service}")
+    String service;
+
+    public String getService() {
+        return service != null ? service : "";
+    }
+
+    public void setService(String service) {
+        System.out.println("Service is " + service);
+        this.service = service;
+    }
+    
+    public String load() {
+        System.out.println("Loading service is '" + getService() + "'");
+        return "/viewRest.xhtml?faces-redirect=true";
+    }
+}
