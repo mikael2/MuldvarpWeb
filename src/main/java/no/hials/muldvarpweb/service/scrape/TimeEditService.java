ff9047d81e6252e9983e50b4d89f7a0f360b46ee
Johan Hessen <johanhess1@gmail.com>
2013-04-05 12:33:49 +0200

    Changed structure in the TimeEditService

--- src/main/java/no/hials/muldvarpweb/service/scrape/TimeEditService.java
+++ src/main/java/no/hials/muldvarpweb/service/scrape/TimeEditService.java
@@ -50,9 +50,9 @@ public class TimeEditService {
     private final static String TIMEEDIT_PARAM_DATE = "wv_ts";
     String lenke = "http://timeedit.hials.no/4DACTION/WebShowSearch/1/1-0?wv_type=5&wv_ts=20130222T141621X6075&wv_search=&wv_startWeek=1301&wv_stopWeek=1318&wv_addObj=&wv_delObj=&wv_obj1=169000&wv_text=Tekstformat";
     //Static variable types
-    private final static String TIMEDIT_PARAM_FORMAT_TEXT = "text";
-    private final static String TIMEDIT_PARAM_FORMAT_TEXT2 = "Tekstformat";
-    private final static String TIMEDIT_PARAM_FORMAT_GRAPHIC = "Grafisk";
+    private final static String TIMEEDIT_VALUE_FORMAT_TEXT = "text";
+    private final static String TIMEEDIT_VALUE_FORMAT_TEXT2 = "Tekstformat";
+    private final static String TIMEEDIT_VALUE_FORMAT_GRAPHIC = "Grafisk";
 
     public TimeEditService() {
     }
@@ -69,7 +69,7 @@ public class TimeEditService {
     public String getURL(String[] objectCodes, String startWeek, String stopWeek) {
 
         String retVal = TIMEEDIT_HIALS_URL
-                + "&" + TIMEDIT_PARAM_FORMAT + "=" + TIMEDIT_PARAM_FORMAT_TEXT
+                + "&" + TIMEDIT_PARAM_FORMAT + "=" + TIMEEDIT_VALUE_FORMAT_TEXT
                 + generateObjectCodeURL(objectCodes);
 
         if (startWeek != null && !startWeek.isEmpty()) {
@@ -92,7 +92,7 @@ public class TimeEditService {
     public String getURL(String[] objectCodes, String date) {
 
         String retVal = TIMEEDIT_HIALS_URL
-                + "&" + TIMEDIT_PARAM_FORMAT + "=" + TIMEDIT_PARAM_FORMAT_TEXT
+                + "&" + TIMEDIT_PARAM_FORMAT + "=" + TIMEEDIT_VALUE_FORMAT_TEXT
                 + generateObjectCodeURL(objectCodes);
 
         if (date != null && !date.isEmpty()) {
@@ -101,8 +101,13 @@ public class TimeEditService {
         return retVal;
     }
     
-    
-    public String generateObjectCodeURL(String[] objectCodes){
+    /**
+     * This method concatenates the TIMEEDIT_PARAM_OBJECT parameter plus a String array of 
+     * objectCodes into one string.
+     * @param objectCodes
+     * @return 
+     */
+    private String generateObjectCodeURL(String[] objectCodes){
         String retVal = "";
         for (int i = 0; i < objectCodes.length; i++) {
             if (i >= MAX_OBJECT_REQUESTS) {
@@ -132,29 +137,6 @@ public class TimeEditService {
         return ui.getQueryParameters();
     }
 
-    /**
-     *
-     * @param id The ID of the Object from which the schedule is to be
-     * retrieved. The Object ID can represent a Course (Fag), a
-     * Programme(Klasse), or a combination.
-     */
-    @GET
-    @Path("object/{objectstring:\\d{6}}{startweek:(/startweek/[^/]+?)?}{stopweek:(/stopweek/[^/]+?)?}")
-    @Produces({MediaType.APPLICATION_JSON})
-    public String getScheduleByObjectID(@PathParam("objectstring") String objectstring,
-            @PathParam("startweek") String startweek,
-            @PathParam("stopweek") String stopweek) {
-
-        //trim away the slashes and names
-        //such is the price for dynamic URL's
-        startweek = startweek.replace("/", "");
-        startweek = startweek.replace("startweek", "");
-        stopweek = stopweek.replace("/", "");
-        stopweek = stopweek.replace("stopweek", "");
-        System.out.println(getURL(objectstring.split("/"), startweek, stopweek));
-
-        return objectstring + startweek + stopweek;
-    }
 
     /**
      *
